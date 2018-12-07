b831054c51f4594611f27625046c30de55a8d748
Alberto J Marin <alberto@ajmar.in>
2018-12-07 12:26:08 -0500

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/git-albertomarin/badx12",
-    version="0.2.1",
+    version="0.2.2",
     zip_safe=False,
 )
